40d02888522869c362f3c0e8788dda53b78f3707
totorookay-maker <totorookay@gmail.com>
2026-07-22 09:10:39 -0700

    build: use relative asset paths for Pages

--- vite.demo.config.ts
+++ vite.demo.config.ts
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   root: resolve(__dirname, 'demo'),
+  base: './',
   plugins: [vue()],
   build: {
     outDir: resolve(__dirname, 'demo-dist'),
