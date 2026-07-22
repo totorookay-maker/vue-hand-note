d9937a1bd92121c3cddf5c4f222b12df4dc418b9
totorookay-maker <totorookay@gmail.com>
2026-07-22 08:41:05 -0700

    Add vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,33 @@
+import { resolve } from 'node:path'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import dts from 'vite-plugin-dts'
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    dts({
+      include: ['src'],
+      insertTypesEntry: true,
+    }),
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'VueHandNote',
+      formats: ['es', 'umd'],
+      fileName: (format) =>
+        format === 'es' ? 'vue-hand-note.js' : 'vue-hand-note.umd.cjs',
+      cssFileName: 'style',
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        exports: 'named',
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
+})
